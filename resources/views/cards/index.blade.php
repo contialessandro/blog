01d15c307856d41e3e3337705df22b5881f701b6
AlessandroConti <contialessandro@hotmail.it>
2016-05-18 14:36:20 +0100

    added style to notes page. New note add form implemented but class not ready yet

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layout')
 
 @section('content')
-<h1>all cards</h1>
-	<div>
-
-		<ul>
-@foreach ($cards as $card)
-
-			<li>{{$card->title}}</li>
-		
-@endforeach
-		</ul>
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <h1>All cards</h1>
+            <ul class="list-group">
+                @foreach ($cards as $card)
+                    <li class="list-group-item"><a href="/cards/{{$card->id}}" >{{$card->title}}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </div>
 @stop
--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -1,16 +1,25 @@
 @extends('layout')
 
 @section('content')
-<h1>{{$card->title}}</h1>
-<ul>
-@foreach ($card->notes as $note)
-	<div>
-
-		
-			<li>{{$note->body}}</li>
-
-@endforeach
-
-
-
+    <div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+            <h1>{{$card->title}}</h1>
+            <ul class="list-group">
+				@foreach ($card->notes as $note)
+					<li class="list-group-item">{{ $note->body }}</li>
+				@endforeach
+			</ul>
+                Add new note
+            <form>
+                <div class="form-group">
+                <textarea name="body" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    </div>
 @stop
