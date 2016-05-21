d03b5f73f707bee7154a570901d5bb0837931965
contialessandro <contialessandro@hotmail.it>
2016-05-21 12:58:30 +0100

    card controller and add new note form working 100%

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -7,7 +7,9 @@
             <h1>{{$card->title}}</h1>
             <ul class="list-group">
 				@foreach ($card->notes as $note)
-					<li class="list-group-item">{{ $note->body }}</li>
+					<li class="list-group-item">{{ $note->body }}
+                        <a href="/notes/{{$note->id}}/edit" class="pull-right">Edit</a>
+                    </li>
 				@endforeach
 			</ul>
                 Add new note
